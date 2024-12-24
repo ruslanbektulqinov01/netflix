962ceffbcd5a27c1f25162473002814be46f1f10
ruslanbektulqinov01 <ruslanbek.tulqinov.01@bk.ru>
2024-12-24 18:27:52 +0500

    added s and m folders

--- netflix/settings.py
+++ netflix/settings.py
@@ -137,7 +137,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles/")
+MEDIA_URL = os.path.join(BASE_DIR, "media/")
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = os.getenv("STATIC_ROOT", default=BASE_DIR / "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
