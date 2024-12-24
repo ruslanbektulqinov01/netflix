6e31ef670da9d5a196afc68ab2ec0a6ca20ae3cd
ruslanbektulqinov01 <ruslanbek.tulqinov.01@bk.ru>
2024-12-24 18:52:38 +0500

    added domain to allowed host and turn off debug

--- netflix/settings.py
+++ netflix/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&5%t80+(+7=md3umx#732x4rayxof5vla0kvyufcvpr7b+p&nt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['netflix.ruslanbek.com']
 
 # Application definition
 
