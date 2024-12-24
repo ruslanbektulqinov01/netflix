d68a12f9c4b3010aabf85a8493c1f162fa9db379
ruslanbektulqinov01 <ruslanbek.tulqinov.01@bk.ru>
2024-12-24 18:56:36 +0500

    .com to .tech

--- netflix/settings.py
+++ netflix/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-&5%t80+(+7=md3umx#732x4rayxof5vla0kvyufcvpr7b+p&nt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['netflix.ruslanbek.com']
+ALLOWED_HOSTS = ['netflix.ruslanbek.tech']
 
 # Application definition
 
